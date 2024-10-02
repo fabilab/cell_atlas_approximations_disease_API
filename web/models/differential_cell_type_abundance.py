2874b54721d554822c618d0ad722e89858dacbf7
Fabio Zanini <fabio.zanini@fastmail.fm>
2024-10-02 10:10:35 +1000

    Differential abundance for separate disease states vs normal

--- web/models/differential_cell_type_abundance.py
+++ web/models/differential_cell_type_abundance.py
@@ -22,26 +22,35 @@ def get_diff_cell_abundance(**filters):
         if "normal" not in disease_states or len(disease_states) < 2:
             continue
 
-        # NOTE: this should be improved
-        meta["disease_bin"] = meta["disease"] != "normal"
-
         table = (
-            meta.groupby(["cell_type", "disease_bin"])["cell_count"]
+            obs.groupby(["cell_type", "disease"])["cell_count"]
             .sum()
-            .unstack("disease_bin", fill_value=0)
+            .unstack("disease", fill_value=0)
         )
-        table.rename(
-            columns={True: "ncells_disease", False: "ncells_normal"}, inplace=True
-        )
-        table["frac_disease"] = table["ncells_disease"] / table["ncells_disease"].sum()
-        table["frac_normal"] = table["ncells_normal"] / table["ncells_normal"].sum()
-        table["delta_frac"] = table["frac_disease"] - table["frac_normal"]
+        table.rename(columns={"normal": "ncell_normal"}, inplace=True)
+        table["frac_normal"] = table["ncell_normal"] / table["ncell_normal"].sum()
         table["dataset_id"] = dataset_id
-        table["ncells_total_disease"] = table["ncells_disease"].sum()
-        table["ncells_total_normal"] = table["ncells_normal"].sum()
-        result.append(table)
+        for state in disease_states:
+            if state == "normal":
+                continue
+            # Slight optimisation whenever there is only one disease state
+            # avoids a copy
+            if len(disease_states) == 2:
+                table_state = table
+            else:
+                table_state = table[
+                    ["dataset_id", state, "ncell_normal", "frac_normal"]
+                ].copy()
+            table_state.rename(columns={state: "ncell_disease"}, inplace=True)
+            table_state["frac_disease"] = (
+                table_state["ncell_disease"] / table_state["ncell_disease"].sum()
+            )
+            table_state["delta_frac"] = (
+                table_state["frac_disease"] - table_state["frac_normal"]
+            )
+        table_state["disease"] = state
+        result.append(table_state)
 
     result = pd.concat(result)
 
     return result
-
