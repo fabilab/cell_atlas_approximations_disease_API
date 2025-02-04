3d2f390e3190d9ea4d19ee851cf96373903b943e
Ying <yingxu0928@gmail.com>
2025-02-05 10:56:00 +1100

    Modify get_metadata to enable multiple value in filters. E.g user can specify cell type to include both T cell and B cell. Similar for other params

--- web/models/metadata.py
+++ web/models/metadata.py
@@ -16,7 +16,7 @@ from models.exceptions import (
 metadata = None
 
 def generate_unique_id(row):
-    """Gnerate SA256 hash for a given row"""
+    """Generate md5 hash (faster than 256) for a given row"""
     row_string = ",".join(map(str, row))  # Convert row values to a single string
     m = hashlib.md5()
     m.update(row_string.encode("utf-8"))  # Update the hash with encoded row string
@@ -44,14 +44,25 @@ def get_metadata(**filters):
     keep = pd.Series(np.ones(len(metadata), dtype=bool), index=metadata.index)
     for key, value in filters.items():
         # Boolean OR
-        invert, value = value.startswith("!"), value.lstrip("!")
+        if type(value) == str:
+            invert, value = value.startswith("!"), value.lstrip("!")
+
+        else:
+            invert = False
+            value = [v.lstrip("!") for v in value]
 
         # Boolean AND between filters
         if key == "sex":
             # "male" is a substring of "female"
             keep_key = metadata[key] == value
         else:
-            keep_key = metadata[key].str.contains(value, case=False)
+            # e.g: ["B cell", "T cell"]
+            if type(value) == list:
+                keep_key = pd.Series(np.zeros(len(metadata), dtype=bool), index=metadata.index)
+                for v in value:
+                    keep_key |= metadata[key].str.contains(v.strip(), case=False)
+            else:
+                keep_key = metadata[key].str.contains(value, case=False)
 
         if invert:
             keep_key = ~keep_key
@@ -60,29 +71,37 @@ def get_metadata(**filters):
     filtered_metadata = metadata.loc[keep]
 
     # Handle errors separately for each parameter
-    if "disease" in filters and metadata["disease"].str.contains(filters["disease"], case=False).sum() == 0:
-        raise DiseaseNotFoundError(
-            msg=f"No disease found that matches '{filters['disease']}'.",
-            disease=filters["disease"]
-        )
+    if "disease" in filters:
+        if (type(filters["disease"]) == str and metadata["disease"].str.contains(filters["disease"], case=False).sum() == 0)\
+        or (type(filters["disease"], list) and all(metadata["disease"].str.contains(v, case=False).sum() == 0 for v in filters["disease"])):
+            raise DiseaseNotFoundError(
+                msg=f"No disease found that matches '{filters['disease']}'.",
+                disease=filters["disease"]
+            )
 
-    if "cell_type" in filters and metadata["cell_type"].str.contains(filters["cell_type"], case=False).sum() == 0:
-        raise CellTypeNotFoundError(
-            msg=f"No cell type found that matches '{filters['cell_type']}'.",
-            cell_type=filters["cell_type"]
-        )
+    if "cell_type" in filters:
+        if (type(filters["cell_type"]) == str and metadata["cell_type"].str.contains(filters["cell_type"], case=False).sum() == 0)\
+        or (type(filters["cell_type"]) == list) and all(metadata["cell_type"].str.contains(v, case=False).sum() == 0 for v in filters["cell_type"]):
+            raise CellTypeNotFoundError(
+                msg=f"No cell type found that matches '{filters['cell_type']}'.",
+                cell_type=filters["cell_type"]
+            )
         
-    if "tissue" in filters and metadata["tissue"].str.contains(filters["tissue"], case=False).sum() == 0:
-        raise TissueNotFoundError(
-            msg=f"No tissue found that matches '{filters['tissue']}'.",
-            tissue=filters["tissue"]
-        )
+    if "tissue" in filters:
+        if (type(filters["tissue"]) == str and metadata["tissue"].str.contains(filters["tissue"], case=False).sum() == 0)\
+        or (type(filters["tissue"]) == list) and all(metadata["tissue"].str.contains(v, case=False).sum() == 0 for v in filters["tissue"]):
+            raise TissueNotFoundError(
+                msg=f"No tissue found that matches '{filters['tissue']}'.",
+                tissue=filters["tissue"]
+            )
 
-    if "development_stage_general" in filters and metadata["development_stage_general"].str.contains(filters["development_stage_general"], case=False).sum() == 0:
-        raise DevelopmentStageNotFoundError(
-            msg=f"No development stage found that matches '{filters['development_stage_general']}'.",
-            development_stage_general=filters["development_stage_general"]
-        )
+    if "development_stage_general" in filters:
+        if (type(filters["development_stage_general"]) == str and metadata["development_stage_general"].str.contains(filters["development_stage_general"], case=False).sum() == 0)\
+        or (type(filters["development_stage_general"]) == list) and all(metadata["development_stage_general"].str.contains(v, case=False).sum() == 0 for v in filters["development_stage_general"]):
+            raise DevelopmentStageNotFoundError(
+                msg=f"No development stage found that matches '{filters['development_stage_general']}'.",
+                development_stage_general=filters["development_stage_general"]
+            )
     
     if filtered_metadata.empty:
         raise NoMatchingDatasetsError(
